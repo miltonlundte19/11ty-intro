aba99c2f9ca0c07c3183731ec91b240a2eb37417
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-10-01 09:35:28 +0200

    ✨ ?

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/images")
-    
+
     eleventyConfig.addWatchTarget("src/images")
+
+    eleventyConfig.addShortcode('image', (src, alt) => '<img alt="&(alt)" src="$(src)">')
 
     return {
         dir: {
